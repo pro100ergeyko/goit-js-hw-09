dce6f25fc97a89a3705b4e53e7b49bb73d85c3d4
pro100ergeyko <124717620+pro100ergeyko@users.noreply.github.com>
2023-06-18 19:00:01 +0300

    add second task

---
+++ src/js/02-timer.js
@@ -0,0 +1,97 @@
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+
+let idInterval = null;
+let selectedDate = null;
+let currentDate = null;
+
+const refs = {
+  dateInput: document.querySelector('input#datetime-picker'),
+  btnStart: document.querySelector('button[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+refs.btnStart.disabled = true;
+refs.btnStart.addEventListener('click', timerStart);
+
+let remainingTime = 0;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    onDateCheck(selectedDates);
+  },
+};
+
+flatpickr(refs.dateInput, options);
+
+Report.info('Greeting!', 'Please, choose a date and click on start');
+
+function onDateCheck(selectedDates) {
+  selectedDate = selectedDates[0].getTime();
+  currentDate = new Date().getTime();
+
+  if (selectedDate > currentDate) {
+    refs.btnStart.disabled = false;
+    Report.success('Congratulation! Click on start!');
+    return;
+  }
+  Report.failure('Ooops...', 'Please, choose a date in the future and');
+}
+
+function timerStart() {
+  idInterval = setInterval(() => {
+    currentDate = new Date().getTime();
+    if (selectedDate - currentDate <= 1000) {
+      clearInterval(idInterval);
+      refs.btnStart.disabled = true;
+      refs.dateInput.disabled = false;
+      Report.info(
+        'Congratulation! Timer stopped!',
+        'Please, if you want to start timer, choose a date and click on start or reload this page'
+      );
+      return;
+    } else {
+      refs.btnStart.disabled = true;
+      refs.dateInput.disabled = true;
+      currentDate += 1000;
+      remainingTime = Math.floor(selectedDate - currentDate);
+      convertMs(remainingTime);
+    }
+  }, 1000);
+}
+
+function createMarkup({ days, hours, minutes, seconds }) {
+  refs.days.textContent = days;
+  refs.hours.textContent = hours;
+  refs.minutes.textContent = minutes;
+  refs.seconds.textContent = seconds;
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+  createMarkup({ days, hours, minutes, seconds });
+  return { days, hours, minutes, seconds };
+}
