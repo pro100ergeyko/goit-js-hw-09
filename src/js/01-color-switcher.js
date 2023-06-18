af8d0c3ff61e6a5a8017e9d8f54fdb62694777bc
pro100ergeyko <124717620+pro100ergeyko@users.noreply.github.com>
2023-06-18 17:01:28 +0300

    finished first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+let idInterval;
+
+refs.btnStop.disabled = true;
+refs.btnStart.addEventListener('click', onBtnStartClick);
+refs.btnStop.addEventListener('click', onBtnStopClick);
+
+function onBtnStartClick(e) {
+  refs.btnStop.disabled = false;
+  refs.btnStart.disabled = true;
+
+  idInterval = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onBtnStopClick(e) {
+  refs.btnStop.disabled = true;
+  refs.btnStart.disabled = false;
+
+  clearInterval(idInterval);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
